4b9e3550c8bfd23935a1454e28bcd8360eaad319
Nuevo Usuario <xcxzczxcxxcx@gmail.com>
2019-12-27 09:55:18 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 var express = require("express");
 var Request = require("request");
 var router = express.Router();
+
+
+
+/* Variables para Fuel SDK */
+const ET_Client = require('sfmc-fuelsdk-node');
+const clientId = '6pc5gwpvanownyydmztsk9vk';
+const clientSecret = 'XqHSQZCdqvSVKV9p6wzdr6tH';
+const stack = 789;
+const client = new ET_Client(clientId, clientSecret, stack);
+
+
 /*Variables para consumir Pushpushgo Service*/
 
 var host ="api.pushpushgo.com";
@@ -14,6 +25,8 @@ var image = "";
 var redirectLink = "";
 /* ----- */
 
+
+/* ----- */
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
@@ -24,7 +37,25 @@ router.get("/form", function(req, res, next) {
 });
 
 router.post('/form',function(req,res,next){
-  Request.post({
+  const deRow = client.dataExtensionRow({
+    Name: 'E301CEDB-B073-448B-B34A-0BA2754D271D',
+    props: ['Nombres'],
+    filter: {}
+    // to return all rows, delete the filter property
+  });
+  
+  deRow.get((err, res) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      for (const result of res.body.Results) {
+        for (const property of result.Properties.Property) {
+          console.log(property);
+        }
+      }
+    }
+  });
+ /* Request.post({
     "headers": { "content-type": "application/json",
                   "X-Token": "771f8f4c-549f-40e3-ac6d-64e8be54d97a" },
     "url": "https://api.pushpushgo.com/project/5dca2b7a8d7dda6c7e30a6c5/send/push",
@@ -46,7 +77,7 @@ router.post('/form',function(req,res,next){
         return console.dir(error);
     }
     console.dir(JSON.parse(body));
-});
+});*/
 res.render('form', {});
 
 });
